e2be38e4fa74055709dc1817f89e36abddc7c344
Xinyao90 <lixinyao90@gmail.com>
2026-01-13 18:41:50 -0600

    Lec2Prep Uploaded

---
+++ untitled/src/lec2Prep/DogLauncher.java
@@ -0,0 +1,11 @@
+package lec2Prep;
+
+public class DogLauncher {
+    static void main(String[] args) {
+//        Dog d = new Dog();
+//        d.size = 25;
+//        d.makeNoise();
+        Dog d = new Dog(51);
+        d.makeNoise();
+    }
+}
